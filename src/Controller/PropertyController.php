38fe20d7243d68089d54df23a4009d044e374a7c
leeandri <rl.tsirih@gmail.com>
2021-11-04 18:33:37 +0300

    7/8-pagination & filter

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,20 +26,33 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/property", name="property.index")
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        //$property[0]->setSold(true);
-        //$this->em->flush();
+        // Create entity for our search minimum room, maximum price
+        // Create form
+        // Manage t3 dans le controller
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
+
+        $properties = $paginator->paginate($this->repository->findAllVisibleQuery($search), $request->query->getInt('page', 1), /*page number*/
+            12 /*limit per page*/);
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties' => $properties,
+            'form' => $form->createView()
         ]);
     }
 
     /**
      * @Route("/property/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      * @param Property $property
+     * @param string $slug
      * @return Response
      */
     public function show(Property $property, string $slug): Response
